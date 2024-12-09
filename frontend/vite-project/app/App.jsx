f2c068239b1fc9e83432ef7d8ec27157febcf64d
Ivogomez03 <ivogomezenrico2015@gmail.com>
2024-12-09 17:35:52 -0300

    Cambios front

--- frontend/vite-project/app/App.jsx
+++ frontend/vite-project/app/App.jsx
@@ -2,37 +2,42 @@ import React from 'react';
 import './App.css'
 import { HashRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 const App = () => {
-    const navigate = useNavigate();
-
-    const goToLogin = () => {
-        navigate('/login');
-    };
-
+    console.log("App cargado correctamente");
     return (
         <div className='conteiner-App'>
             <header>
                 <h1>Bienvenido al Sistema de Delivery</h1>
             </header>
             <section className='seccion-App'>
-                <div className="seccion-App-cols">
-                    <h2>Gestión de horarios</h2>
-                    <img src="/schedule.png" alt="Gestion de horarios" />
-                </div>
-                <div className="seccion-App-cols">
-                    <h2>Organización eficiente</h2>
-                    <img src="/diagram.png" alt="Organización" />
-                </div>
-                <div className="seccion-App-cols">
-                    <h2>Reserva de aula</h2>
-                    <img src="/reserved.png" alt="Reserva" />
-                </div>
+                <img src="/comida.jpg" alt="" />
+                <div className='seccion-botones-app'>
+                    <div className='sub-seccion-botones-app'>
+                        <button className="botones-navegacion">
+                            Vendedores
+                        </button>
+                        <button className="botones-navegacion">
+                            Clientes
+                        </button>
+                    </div>
+                    <div className='sub-seccion-botones-app'>
+                        <button className="botones-navegacion">
+                            Items Menú
+                        </button>
+                        <button className="botones-navegacion">
+                            Pedidos
+                        </button>
+                    </div>
 
+
+
+
+                </div>
 
             </section >
             <footer className='footer-App'>
                 <div className="footer-col">
                     <h3>Acerca de</h3>
-                    <p>Este sistema permite gestionar reservas de aulas de manera eficiente y rápida.</p>
+                    <p>Este sistema permite realizar pedidos de manera eficiente y rápida.</p>
                 </div>
                 <div className="footer-col">
                     <h3>Redes Sociales</h3>
@@ -40,7 +45,7 @@ const App = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                         </svg>
-                        <a href="#">LinkedIn Mario</a>
+                        <a href="#">LinkedIn Pablo</a>
                     </div>
                     <div className="footer-col-redes">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
@@ -52,20 +57,26 @@ const App = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                         </svg>
-                        <a href="#">LinkedIn Tomi</a>
+                        <a href="#">LinkedIn Tobi</a>
                     </div>
                     <div className="footer-col-redes">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                         </svg>
-                        <a href="#">LinkedIn Migue</a>
+                        <a href="#">LinkedIn Fran</a>
+                    </div>
+                    <div className="footer-col-redes">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                        </svg>
+                        <a href="#">LinkedIn Franco</a>
                     </div>
 
                 </div>
                 <div className="footer-col">
                     <h3>Créditos</h3>
                     <p>Desarrollado por el equipo de Ingeniería UTN</p>
-                    <p>&copy; 2024 Gestión de Reservas</p>
+                    <p>&copy; 2024 Gestión de Pedidos</p>
                 </div>
             </footer>
 
@@ -78,27 +89,7 @@ const MainApp = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<App />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/login/bienvenidoAdmin/RegistrarBedel" element={<RegistrarBedel />} />
-                <Route path="/login/bienvenidoAdmin/BuscarBedel" element={<BuscarBedel />} />
-                <Route path="/login/bienvenidoAdmin/BuscarBedel/ModificarBedel" element={<ModificarBedel />} />
-                <Route path="/login/bienvenidoAdmin/BuscarBedel/EliminarBedel" element={<EliminarBedel />} />
-                <Route path="/login/bienvenidoAdmin/BuscarBedel/ListaBedeles" element={<ListaBedeles />} />
-                <Route path="/login/bienvenidoAdmin" element={<BienvenidoAdmin />} />
-                <Route path="/login/bienvenidoBedel" element={<BienvenidoBedel />} />
-                <Route path="/login/bienvenidoBedel/TipoReserva" element={<TipoDeReserva />} />
-                <Route path="/login/bienvenidoBedel/BuscarReservasDE" element={<BuscarReservasDE />} />
-                <Route path="/login/bienvenidoBedel/BuscarReservasPC" element={<BuscarReservasPC />} />
-                <Route path="/login/bienvenidoBedel/BuscarAulas" element={<BuscarAulas />} />
-                <Route path="/login/bienvenidoBedel/BuscarAulas/ListaAulas" element={<ListaAulas />} />
-                <Route path="/login/bienvenidoBedel/BuscarAulas/ListaAulas/ModificarAulaMultimedio" element={<ModificarAulaMultimedio />} />
-                <Route path="/login/bienvenidoBedel/BuscarAulas/ListaAulas/ModificarAulaSinRecursosAdicionales" element={<ModificarAulaSRA />} />
-                <Route path="/login/bienvenidoBedel/BuscarAulas/ListaAulas/ModificarAulaInformatica" element={<ModificarAulaInformatica />} />
-                <Route path="/login/RegistrarReservaPeriodica" element={<RegistrarReservaP />} />
-                <Route path="/login/ReservaClaseP" element={<ReservaClaseP />} />
-                <Route path="/login/RegistrarReservaEsporadica" element={<RegistrarReservaE />} />
-                <Route path="/login/ReservaClaseE" element={<ReservaClaseE />} />
-                <Route path="/login/CoincidenDyH" element={<CoincidenDyH />} />
+
             </Routes>
         </Router>
     );
